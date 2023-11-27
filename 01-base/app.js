451f71564d8ad52914520a7e3f138c00778c6165
Tigran Vartanyan <tvartanyan29@yandex.ru>
2023-11-27 13:27:57 +0400

    написан и подключен JS

---
+++ 01-base/app.js
@@ -0,0 +1,29 @@
+let num = 42 
+let firstName = 'Tigran'
+const isProgrammer = true
+
+/* Can do
+let $ = 'test'
+let $num = 42
+let num$ = 42
+let _ = 49
+let _num = 12
+let num_ = 12
+let first_name = 'Elena' // bad
+let myNum = 34 // good (Camel Case)
+let num42 = 10
+*/
+
+/* Restricted (запрещенные названия переменных)
+let 42num = '11' // переменная начинается на цифры
+let my-num = 1 // переменная через дефис
+let const // зарезервированные слова 
+*/
+
+firstName = 'Nane'
+// isProgrammer = false // error because of const
+
+// вывод результатов
+
+// alert(firstName)
+// console.log('Test:', num, isProgrammer)
