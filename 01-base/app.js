71b3d7a8dcff0484ec3a2b56b9b4bea89ea485f6
Tigran Vartanyan <tvartanyan29@yandex.ru>
2023-12-05 14:43:05 +0400

    Работающий калькулятор

--- 01-base/app.js
+++ 01-base/app.js
@@ -44,5 +44,58 @@ let const // зарезервированные слова
 // console.log(num3)
 
 // const fullName = firstName + ' Vartanyan'
-const fullName = firstName + ' ' + 'Vartanyan'
-console.log(fullName)
+// const fullName = firstName + ' ' + 'Vartanyan'
+// console.log(fullName)
+
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+let action = '+'
+
+// console.log(resultElement.textContent)
+// resultElement.textContent = (42 - 2) / num
+ 
+// console.log(typeof sum)
+
+plusBtn.onclick = function () {
+    action = '+'
+}
+
+minusBtn.onclick = function () {
+    action = '-'
+}
+
+function printResult(result) {
+    if (result < 0 ) {
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'
+    }
+    resultElement.textContent = result
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    // if (actionSymbol == '+') {
+    //     return num1 + num2
+    // } else if (actionSymbol == '-') {
+    //     return num1 - num2
+    // }
+    return actionSymbol = '+' ? num1 + num2 : num1 - num2
+}
+
+submitBtn.onclick = function () {
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
+//     if (action == '+') {
+//         const sum =  Number(input1.value) + Number(input2.value)
+//         printResult(sum)
+//     } else if (action == '-') {
+//         const sum =  Number(input1.value) - Number(input2.value)
+//         printResult(sum)
+//     }
+}
